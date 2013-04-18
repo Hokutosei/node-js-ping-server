28f6a6fee23f00ec4a1386d3988a5d7a8a543b9a
jeane paul <jeanepaul@gmail.com>
2013-04-19 02:35:52 +0900

    push before sleep

--- fujico-ping-server/work-ping.js
+++ fujico-ping-server/work-ping.js
@@ -1,10 +1,14 @@
-var app = require('http').createServer(handler),
+//var app = require('express').createServer(),
+var express = require('express'),
+    app = express(),
     fs = require('fs'),
-    io = require('socket.io').listen(app);
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
 var port = 8888, localhost = '0.0.0.0';
 
-app.listen(port, localhost);
+//app.listen(port, localhost);
 
 function handler(req, res) {
   fs.readFile(__dirname + '/index.html', function(err, data) {
@@ -17,56 +21,62 @@ function handler(req, res) {
   })
 }
 
-//io.sockets.on('connection', function(socket){
-//	console.log(app);
-//  socket.emit('news', { hello: 'jeanepaul' });
-//  socket.on('my_event', function(data) {
-//  	console.log(data);
-//  });
-//});
+app.get(/^(.+)$/, function(req, res) {
+    res.sendfile('public/' + req.params[0]);
+});
 
 
-function hosts() {
-    var url = ['hitobank.herokuapp.com', 'hitobank-test.herokuapp.com']
-    return url
+function myHosts() {
+    return url = ['hitobank.herokuapp.com', 'hitobank-test.herokuapp.com']
 }
 
-var myHosts = {
+var myHostsOptions = {
     url: function(counter) {
-        return hosts()[counter];
+        return myHosts()[counter];
     },
-    lengthHosts: function() {
-        return hosts().length
+    hostsLength: function() {
+        return myHosts().length
+    },
+    delay: function() {
+        return delay = 2000;
     }
 }
 
-var delay = 29000
+var x;
+io.sockets.on('connection', function(socket) {
+        socket.emit('server_message', {msg: x})
+});
 
-var timer = setTimeout(test, delay);
+var dataArray = [];
 
-var counter = 0;
-function test(url) {
-    for(var i = 0; i < myHosts.lengthHosts(); i++) {
-        (function(i) {
-            http.get('http://' + myHosts.url(i), function(res) {
-                console.log('==========================================')
-                console.log('sending request to.. ' + myHosts.url(i))
-                console.log('status code: ' + res.statusCode);
-                console.log('headers:' + JSON.stringify(res.headers))
-                header = JSON.stringify(res.headers)
-                console.log('==========================================')
-            }).on('error', function(e) {
-                    console.log(e.message)
-                })
-        })(i)
-    }
-    var timer = setTimeout(test, delay)
-    counter++;
-    console.log(counter)
+for(var i = 0; i < myHostsOptions.hostsLength(); i++) {
+    (function(i) {
+        dataArray.length = 0;
+        setTimeout(makeRequest, myHostsOptions.delay());
+        function makeRequest() {
+            http.get('http://' + myHostsOptions.url(i), function(res) {
+                console.log('==========================================');
+                console.log('sending request to.. ' + myHostsOptions.url(i));
+//                console.log('status code: ' + res.statusCode);
+//                console.log('headers:' + JSON.stringify(res.headers))
+//                header = JSON.stringify(res.headers)
+//                console.log('==========================================')
+//                x = myHostsOptions.url(i);
+//                io.sockets.emit('server_msg', { host: myHostsOptions.url(i) });
+            });
+            setTimeout(makeRequest, myHostsOptions.delay());
+            dataArray.push(myHostsOptions.url(i));
+            io.sockets.emit('server_msg', { host: myHostsOptions.url(i), data: dataArray });
+        }
+    })(i);
 }
 
+server.listen(port);
+
+console.log('Server is running at ' + localhost + '...' + port)
 
 
 
-console.log('Server is running at ' + localhost + '...')
+
+
 
